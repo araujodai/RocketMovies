c88ede435ace6a5964b1f08f59c26686be7ca3a3
daiane <daiane.ajmachado@gmail.com>
2023-04-24 18:35:38 -0300

    update button component

--- src/components/Button/styles.js
+++ src/components/Button/styles.js
@@ -1,3 +1,24 @@
 import styled from "styled-components";
 
-export const Container = styled.button``;
+export const Container = styled.button`
+  width: 100%;
+  background-color: ${({ theme }) => theme.COLORS.PRIMARY_COLOR};
+  border-radius: 1rem;
+  padding-block: 1.6rem;
+
+  color: ${({ theme }) => theme.COLORS.GRAY_400};
+  font-size: 1.6rem;
+  font-weight: 500;
+
+  &:disabled {
+    opacity: 0.5;
+
+    :hover {
+      filter: none;
+      cursor: not-allowed;
+    }
+   
+  }
+
+
+`;
