b2400b4b76e6a896081870bc96c0cc72dc276ed1
daiane <daiane.ajmachado@gmail.com>
2023-05-08 18:59:33 -0300

    update Home

--- src/pages/Home/index.jsx
+++ src/pages/Home/index.jsx
@@ -1,73 +1,67 @@
 import { FiPlus } from "react-icons/fi";
 
 import { Header } from "../../components/Header";
-import { Content } from "../../components/Content";
+import { ContentWrapper } from "../../components/ContentWrapper";
 import { Movie } from "../../components/Movie";
 
-import { Container, NewMovie, HeaderContent } from "./styles";
+import { Container, TitleWrapper, NewMovie } from "./styles";
 
 export function Home() {
   return (
     <Container>
       <Header />
-      <HeaderContent>
-        <h1>Meus filmes</h1>
-        <NewMovie>
-          <FiPlus />
-          Adicionar filme
-        </NewMovie>
-      </HeaderContent>
 
-      <Content>
-        <Movie data={{
-          title: "Interestellar",
-          description: `
-          Pragas nas colheitas fizeram a civilização humana regredir para uma sociedade agrária em futuro de data desconhecida. Cooper, ex-piloto da NASA, tem uma fazenda com sua família. Murphy, a filha de dez anos de Cooper, acredita que seu quarto está assombrado por um fantasma que tenta se comunicar com ela. Pai e filha descobrem que o "fantasma" é uma inteligência desconhecida.
-          `,
-          tags: [
-            { id: "1", name: "scifi"},
-            { id: "2", name: "drama"},
-            { id: "3", name: "aventura"}
-          ]
-        }} />
+      <main>
+        <TitleWrapper>
+          <h1>Meus filmes</h1>
 
-        {/* <Movie data={{
-          title: "Interestellar",
-          description: `
-          Pragas nas colheitas fizeram a civilização humana regredir para uma sociedade agrária em futuro de data desconhecida. Cooper, ex-piloto da NASA, tem uma fazenda com sua família. Murphy, a filha de dez anos de Cooper, acredita que seu quarto está assombrado por um fantasma que tenta se comunicar com ela. Pai e filha descobrem que o "fantasma" é uma inteligência desconhecida.
-          `,
-          tags: [
-            { id: "1", name: "sci-fi"},
-            { id: "2", name: "drama"},
-            { id: "3", name: "aventura"}
-          ]
-        }} />
+          <NewMovie>
+            <FiPlus size={16} />
+            Adicionar filme
+          </NewMovie>
+        </TitleWrapper>
 
-        <Movie data={{
-          title: "Interestellar",
-          description: `
-          Pragas nas colheitas fizeram a civilização humana regredir para uma sociedade agrária em futuro de data desconhecida. Cooper, ex-piloto da NASA, tem uma fazenda com sua família. Murphy, a filha de dez anos de Cooper, acredita que seu quarto está assombrado por um fantasma que tenta se comunicar com ela. Pai e filha descobrem que o "fantasma" é uma inteligência desconhecida.
-          `,
-          tags: [
-            { id: "1", name: "sci-fi"},
-            { id: "2", name: "drama"},
-            { id: "3", name: "aventura"}
-          ]
-        }} />
+        <ContentWrapper>
 
-        <Movie data={{
-          title: "Interestellar",
-          description: `
-          Pragas nas colheitas fizeram a civilização humana regredir para uma sociedade agrária em futuro de data desconhecida. Cooper, ex-piloto da NASA, tem uma fazenda com sua família. Murphy, a filha de dez anos de Cooper, acredita que seu quarto está assombrado por um fantasma que tenta se comunicar com ela. Pai e filha descobrem que o "fantasma" é uma inteligência desconhecida.
-          `,
-          tags: [
-            { id: "1", name: "sci-fi"},
-            { id: "2", name: "drama"},
-            { id: "3", name: "aventura"}
-          ]
-        }} /> */}
+          <Movie data={{
+            title: "Interestellar",
+            description: `
+            Pragas nas colheitas fizeram a civilização humana regredir para uma sociedade agrária em futuro de data desconhecida. Cooper, ex-piloto da NASA, tem uma fazenda com sua família. Murphy, a filha de dez anos de Cooper, acredita que seu quarto está assombrado por um fantasma que tenta se comunicar com ela. Pai e filha descobrem que o "fantasma" é uma inteligência desconhecida.
+            `,
+            tags: [
+              { id: "1", name: "scifi" },
+              { id: "2", name: "drama" },
+              { id: "3", name: "aventura" }
+            ]
+          }} />
 
-      </Content>
+          <Movie data={{
+            title: "Interestellar",
+            description: `
+            Pragas nas colheitas fizeram a civilização humana regredir para uma sociedade agrária em futuro de data desconhecida. Cooper, ex-piloto da NASA, tem uma fazenda com sua família. Murphy, a filha de dez anos de Cooper, acredita que seu quarto está assombrado por um fantasma que tenta se comunicar com ela. Pai e filha descobrem que o "fantasma" é uma inteligência desconhecida.
+            `,
+            tags: [
+              { id: "1", name: "scifi" },
+              { id: "2", name: "drama" },
+              { id: "3", name: "aventura" }
+            ]
+          }} />
+
+          <Movie data={{
+            title: "Interestellar",
+            description: `
+            Pragas nas colheitas fizeram a civilização humana regredir para uma sociedade agrária em futuro de data desconhecida. Cooper, ex-piloto da NASA, tem uma fazenda com sua família. Murphy, a filha de dez anos de Cooper, acredita que seu quarto está assombrado por um fantasma que tenta se comunicar com ela. Pai e filha descobrem que o "fantasma" é uma inteligência desconhecida.
+            `,
+            tags: [
+              { id: "1", name: "scifi" },
+              { id: "2", name: "drama" },
+              { id: "3", name: "aventura" }
+            ]
+          }} />
+
+        </ContentWrapper>
+      </main>
     </Container>
   );
 };
+
--- src/pages/Home/styles.js
+++ src/pages/Home/styles.js
@@ -1,60 +1,62 @@
-import { Button } from "../../components/Button";
 import styled from "styled-components";
 
 export const Container = styled.div`
   width: 100%;
   height: 100vh;
+
   display: grid;
-  grid-template-rows: 11.6rem 13.1rem auto;
-  /* grid-template-rows: 11.6rem auto auto; */
-  grid-template-columns: auto minmax(33rem, 113.7rem) auto;
+  grid-template-rows: 11.6rem auto;
   grid-template-areas: 
-  "header header header"
-  ". headerContent ."
-  ". main ."
-  ;
+  "header"
+  "main";
 
   > main {
-    /* margin-top: 3.7rem; */
-    background: green;
-/* 
-    article + article {
-      margin-bottom: 2.4rem;
-    } */
+    grid-area: main;
 
-    display: flex;
-    flex-direction: column;
-    gap: 2.4rem;
-    align-self: start;
-  
+    display: grid;
+    grid-template-rows: 4.8rem auto;
+    grid-template-columns: auto minmax(33rem, 113.7rem) auto;
+    grid-template-areas: 
+    ". TitleWrapper ."
+    ". ContentWrapper .";
+
+    overflow: hidden;
+    padding: 4.7rem 0 4rem 0;
+
+
+    > div:nth-child(2) {
+      margin-top: 3.7rem;
+      display: flex;
+      flex-direction: column;
+      gap: 2.4rem;
+    }
   }
-  > header {
-    position: absolute;
-  }
-
-  padding-bottom: 4rem;
-
 `;
 
-export const NewMovie = styled(Button)`
-  padding: 3.2rem;
-  max-width: 20.7rem;
-  
-`;
-export const HeaderContent = styled.div`
-    height: 4.8rem;
-    grid-area: headerContent;
-    background: red;
+export const TitleWrapper = styled.div`
+  grid-area: TitleWrapper;
 
-    display: flex;
-    align-items: center;
-    justify-content: space-between;
-    margin-top: 4.7rem;
-    border: 1px solid red;
-    position: relative;
+  display: flex;
+  align-items: center;
+  justify-content: space-between;
 
-    h1 {
+  h1 {
       font-size: 3.2rem;
       font-weight: 400;
     }
+`;
+
+export const NewMovie = styled.button`
+  background-color: ${({ theme }) => theme.COLORS.PRIMARY_COLOR};
+  color: ${({ theme }) => theme.COLORS.BACKGROUND_800};
+
+  border: none;
+  border-radius: 8px;
+  padding: 1.3rem 3.2rem;
+
+  font-size: 1.6rem;
+
+  display: flex;
+  align-items: center;
+  gap: 0.8rem;
 `;
