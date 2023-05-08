ca26376f17a61975913e2643f420b9fdd8693d8a
daiane <daiane.ajmachado@gmail.com>
2023-05-08 19:54:45 -0300

    create routes

--- src/main.jsx
+++ src/main.jsx
@@ -5,19 +5,13 @@ import { ThemeProvider } from "styled-components";
 import GlobalStyles from "./styles/global";
 import theme from "./styles/theme";
 
-import { Preview } from "./pages/Preview";
-import { Details } from "./pages/Details";
-import { Profile } from "./pages/Profile";
-import { Home } from "./pages/Home";
-import { SignIn } from "./pages/SignIn";
-import { SignUp } from "./pages/SignUp";
-import { New } from "./pages/New";
+import { Routes } from "./routes";
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <ThemeProvider theme={theme}>
       <GlobalStyles />
-      <New />
+      <Routes />
     </ThemeProvider>
   </React.StrictMode>,
 )
