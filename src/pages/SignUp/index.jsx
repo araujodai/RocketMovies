02fae3bbda744609473e91c62570b41bdccdfef6
daiane <daiane.ajmachado@gmail.com>
2023-06-26 18:39:31 -0300

    feat: create and register user in database

--- src/pages/SignUp/index.jsx
+++ src/pages/SignUp/index.jsx
@@ -1,5 +1,8 @@
+import { useState } from "react";
 import { FiUser, FiMail, FiLock, FiArrowLeft } from "react-icons/fi";
-import { Link } from "react-router-dom";
+import { Link, useNavigate } from "react-router-dom";
+
+import { api } from "../../services/api";
 
 import { Input } from "../../components/Input";
 import { Button } from "../../components/Button";
@@ -7,6 +10,32 @@ import { Button } from "../../components/Button";
 import { Container, Form, Background } from "./styles";
 
 export function SignUp() {
+  const [name, setName] = useState("");
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+
+  const navigate = useNavigate();
+
+  function handleSignUp() {
+    if (!name || !email || !password) {
+      return alert("Preencha todos os campos para se cadastrar.")
+    };
+
+    api.post("/users", { name, email, password })
+    .then(() => {
+      alert("Cadastro realizado com sucesso.")
+      navigate("/")
+    })
+    .catch(error => {
+      if (error.response) {
+        alert(error.response.data.message);
+      } else {
+        alert("Não foi possível cadastrar este usuário, verifique e tente novamente.")
+      }
+    })
+
+  };
+
   return (
     <Container>
       <Form>
@@ -19,21 +48,24 @@ export function SignUp() {
             placeholder="Nome"
             type="text"
             icon={FiUser}
+            onChange={e => setName(e.target.value)}
           />
 
           <Input
             placeholder="E-mail"
             type="text"
             icon={FiMail}
+            onChange={e => setEmail(e.target.value)}
           />
 
           <Input
             placeholder="Senha"
             type="password"
             icon={FiLock}
+            onChange={e => setPassword(e.target.value)}
           />
 
-          <Button title="Cadastrar" />
+          <Button title="Cadastrar" onClick={handleSignUp} />
         </fieldset>
 
         <Link to="/">
