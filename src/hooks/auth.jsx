82064952429465be3a7d7a11205db9b13b4f6355
daiane <daiane.ajmachado@gmail.com>
2023-06-29 16:25:47 -0300

    feat: save JWT token in local Storage

--- src/hooks/auth.jsx
+++ src/hooks/auth.jsx
@@ -1,4 +1,4 @@
-import { createContext, useContext, useState } from "react";
+import { createContext, useContext, useState, useEffect } from "react";
 
 import { api } from "../services/api";
 
@@ -8,11 +8,15 @@ function AuthProvider({ children }) {
   const [data, setData] = useState({});
 
   async function signIn({ email, password }) {
+
     try {
       const response = await api.post("/sessions", { email, password });
       const { user, token } = response.data;
+
+      localStorage.setItem("@rocketmovies:user", JSON.stringify(user));
+      localStorage.setItem("@rocketmovies:token", token);
       
-      api.defaults.headers.authorization(`Bearer ${token}`);
+      api.defaults.headers.authorization = `Bearer ${token}`;
       setData({ user, token });
 
     } catch (error) {
@@ -24,6 +28,21 @@ function AuthProvider({ children }) {
 
     };
   };
+
+  useEffect(() => {
+    const token = localStorage.getItem("@rocketmovies:token");
+    const user = localStorage.getItem("@rocketmovies:user");
+
+    if (token && user) {
+      api.defaults.headers.authorization = `Bearer ${token}`;
+
+      setData({
+        token,
+        user: JSON.parse(user)
+      });
+    };
+
+  }, []);
 
   return (
     <AuthContext.Provider value={{ signIn, user: data.user }}>
