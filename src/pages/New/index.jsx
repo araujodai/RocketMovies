45f9e3995032c02e89c7eaa03da88f57e806f440
daiane <daiane.ajmachado@gmail.com>
2023-07-03 18:40:33 -0300

    feat: remove movie tags

--- src/pages/New/index.jsx
+++ src/pages/New/index.jsx
@@ -20,6 +20,10 @@ export function New() {
     setNewTag("");
   };
 
+  function handleRemoveTag(deleted) {
+    setTags(prevState => prevState.filter(tag => tag !== deleted));
+  };
+
   return (
     <Container>
       <Header />
@@ -50,7 +54,7 @@ export function New() {
                     <MovieItem 
                       key={String(index)}
                       value={tag}
-                      onClick={() => {}}
+                      onClick={() => handleRemoveTag(tag)}
                     />
                   ))
                 }
